3b19171432a13fb8b0b57040dacb862c6d67b0fc
mr-karan <karansharma1295@gmail.com>
2019-04-28 15:56:00 +0530

    fix: bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def requirements():
 
 setup(
     name="swiggy_analytics",
-    version="1.6",
+    version="1.6.1",
     description="A CLI for fetching past orders from Swiggy and performing basic stats in the terminal",
     long_description=read_readme(),
     # long_description_content_type='text/markdown',
